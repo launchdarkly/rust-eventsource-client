17c73ea788e8fdfaa597d102aec4aa46fcc24eb1
Casey Waldren <cwaldren@launchdarkly.com>
2022-05-06 12:05:27 -0700

    Update to cimg/rust:1.60.0 and remove nightly toolchain (#35)
    Code coverage is stabilizing in 1.60.0 and no longer requires a nightly toolchain.

--- coverage.sh
+++ coverage.sh
@@ -3,13 +3,7 @@
 set -o errexit
 set -o pipefail
 
-# We are using an older version of the toolchain for now as there is a bug with
-# coverage generation. See https://github.com/rust-lang/rust/issues/79645
-export RUSTUP_TOOLCHAIN=nightly-2022-01-09
-
-# cargo-llvm-cov requires the nightly compiler to be installed
-rustup toolchain install $RUSTUP_TOOLCHAIN
-rustup component add llvm-tools-preview --toolchain $RUSTUP_TOOLCHAIN
+rustup component add llvm-tools-preview
 cargo install cargo-llvm-cov
 
 # generate coverage report to command line by default; otherwise allow
